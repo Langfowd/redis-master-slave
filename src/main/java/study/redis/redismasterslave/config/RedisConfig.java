8856bded9c904e1c7d9f25d95ec6a35c19608869
xiaoluo <luo_tian_fu@163.com>
2019-10-14 14:50:47 +0800

    Initial commit

---
+++ src/main/java/study/redis/redismasterslave/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package study.redis.redismasterslave.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("#{'${spring.redis.sentinel.nodes}'.split(',')}")
+    private List<String> nodes;
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis")
+    public JedisPoolConfig getRedisConfig(){
+        return new JedisPoolConfig();
+    }
+    @Bean
+    public RedisSentinelConfiguration sentinelConfiguration(){
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration();
+        //配置matser的名称
+        redisSentinelConfiguration.master("mymaster");
+        //配置redis的哨兵sentinel
+        Set<RedisNode> redisNodeSet = new HashSet<>();
+        nodes.forEach(x->{
+            redisNodeSet.add(new RedisNode(x.split(":")[0],Integer.parseInt(x.split(":")[1])));
+        });
+        redisSentinelConfiguration.setSentinels(redisNodeSet);
+        return redisSentinelConfiguration;
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate<String,String> redisTemplate = new StringRedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        return redisTemplate;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig, RedisSentinelConfiguration sentinelConfig) {
+        return new JedisConnectionFactory(sentinelConfig,jedisPoolConfig);
+    }
+}
